b31ca0afedceb4480b4c8e283f07f59ca56caf6e
soni dhara <sonidhara915@gmail.com>
2026-06-25 16:33:56 +0530

    Add files via upload

---
+++ python_opretors.py.py
@@ -0,0 +1,107 @@
+#Arithmetical Opretors
+'''
+a=10
+b=20
+
+print("Addition: ",a+b)
+print("Substraction: ",a-b)
+print("Multification: ",a*b)
+print("division:",a/b)
+print("Modulas: ",a%b)
+print("Expontiation: ",a**b)
+print("Floor divison:",a//b)
+'''
+
+#Assingment Operators
+
+#1) Simple assingnment
+'''
+x=5
+print(x)
+'''
+
+#2) Add assingnment
+'''
+x=5
+y=2
+
+x+=y
+print(x)
+print(y)
+
+x-=y
+print(x)
+print(y)
+
+x*=y
+print(x)
+print(y)
+
+x/=y
+print(x)
+print(y)
+
+x%=y
+print(x)
+print(y)
+
+x**=y
+print(x)
+print(y)
+
+x//=y
+print(x)
+print(y)
+'''
+
+
+#3)comparision operators
+'''
+x=10
+y=10
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x>=y)
+print(x<y)
+print(x<=y)
+'''
+
+#4)logical Operators(and,or,not)
+'''
+x=True
+y=False
+z=False
+
+print(x and y)
+print(y and z)
+print(x or y)
+print(y or z)
+print(not x)
+print(not y)
+print(not z)
+'''
+
+#5) multiple Assingments
+'''
+x,y,z=10,20,15
+print(x,y,z)
+'''
+#6) ID Operatos
+'''
+a=10
+print(id(a))
+'''
+
+# Type conversion
+
+num="123"
+print(type(num))
+
+print(int(num))
+print(int(num)+7)
+
+
+
+
