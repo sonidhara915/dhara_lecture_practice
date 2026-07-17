5b880808cadca9157a0f15c2e800870ec9293c9d
soni dhara <sonidhara915@gmail.com>
2026-07-17 17:15:32 +0530

    Add files via upload

---
+++ Arrey.py
@@ -0,0 +1,21 @@
+# Array in Python
+
+# Create a 1D Array (list) with five integer element.
+
+# 2 Fector of defined in Array
+
+# Homogeneous Array
+
+num = [1 , 2 , 3 , 4 , 5]
+
+print("Array Element:")
+print(type(num))
+
+for i in num:
+    print(i)
+
+# Heterogeneous Array
+
+student = ["vivek" , 25 , 89 , True]
+
+print(student)
